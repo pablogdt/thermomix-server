24d9a61a0656062054e036bd997a0e8ebf3ec3fc
Unknown <pablogdt@gmail.com>
2018-06-08 09:39:25 +0200

    calorias, warnings y vegetariano

--- src/main/java/es/pablogdt/thermomix/model/Recipe.java
+++ src/main/java/es/pablogdt/thermomix/model/Recipe.java
@@ -3,6 +3,7 @@ package es.pablogdt.thermomix.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -17,9 +18,38 @@ public class Recipe {
     @Enumerated(EnumType.STRING)
     private RecipeCategory category;
 
+    @Transient
+    private Integer calories = 0;
+
+    @Transient
+    private List<String> warnings = new ArrayList<>();
+
+    @Transient
+    private boolean isVegetarian = true;
+
 //    @ManyToMany
 //    private Set<RecipeIngredient> recipeIngredients;
 
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)//(mappedBy = "recipe")
     private List<Step> steps;
+
+    @PostLoad
+    public void calculateAge() {
+        for (Step step : steps) {
+            for (RecipeIngredient recipeIngredient : step.getRecipeIngredientsToAdd()) {
+                //Vegetarian calculation
+                if (!recipeIngredient.getIngredient().isVegetarianFriendly()) {
+                    isVegetarian = false;
+                }
+                //Calories calculation
+                if (recipeIngredient.getAmountType().equals(AmountType.GRAMES)) {
+                    calories += Math.round(recipeIngredient.getAmount() / 100.0f * recipeIngredient.getIngredient().getCaloriesPer100Gr());
+                } else {
+                    if (recipeIngredient.getIngredient().getCaloriesPer100Gr() > 0) {
+                        warnings.add(recipeIngredient.getIngredient().getName() + " calories not added because it has as amount type " + recipeIngredient.getAmountType() + " (not GRAMES)");
+                    }
+                }
+            }
+        }
+    }
 }
